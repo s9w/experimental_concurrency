6d76f318ed863e0ef6b0457c371b20fbcd255f22
Sebastian Werhausen <swerhausen@gmail.com>
2021-12-12 21:02:41 +0100

    reusing the same thread to make this all much faster

--- semaphore_latency.cpp
+++ semaphore_latency.cpp
@@ -13,25 +13,35 @@ namespace {
       std::chrono::high_resolution_clock::time_point t1{};
       int processor_number{};
    };
+   std::atomic_bool should_end = false;
+   std::atomic_bool armed = false;
    std::atomic<std::optional<thread_result>> t1;
 
 
-   auto thread_fun() -> void {
-      semaphore.acquire();
-      const auto time = std::chrono::high_resolution_clock::now();
-      const auto x = static_cast<int>(GetCurrentProcessorNumber());
-      t1.store(thread_result{ time, x });
+   auto thread_fun() -> void
+   {
+      while(should_end.load() == false)
+      {
+         armed.wait(false);
+
+         semaphore.acquire();
+         const auto time = std::chrono::high_resolution_clock::now();
+
+         t1.store(thread_result{ time, 42 });
+         armed.store(false);
+      }
+      std::cout << "ended\n";
    }
 
    auto measure() -> paired_time {
-      // startup thread
-      std::jthread j(thread_fun);
-      std::this_thread::sleep_for(max_threadup_spinup_time);
+      armed.store(true);
+      armed.notify_one();
+      std::this_thread::sleep_for(max_thread_write_time);
 
-      // Signal thread and time
       const auto t0 = std::chrono::high_resolution_clock::now();
       semaphore.release();
       std::this_thread::sleep_for(max_thread_write_time);
+
       
       if (t1.load().has_value() == false)
          std::terminate();
@@ -40,19 +50,26 @@ namespace {
       const double ns = std::chrono::duration_cast<dbl_ns>(loaded->t1 - t0).count();
 
       t1.store(std::nullopt);
-      return paired_time{ static_cast<int>(GetCurrentProcessorNumber()), loaded->processor_number, ns };
+      const auto processor_number = static_cast<int>(GetCurrentProcessorNumber());
+      return paired_time{ processor_number, loaded->processor_number, ns };
    }
 
 }
 
 auto measure_semaphore_latency(const int n) -> void {
+   std::jthread j(thread_fun);
+   std::this_thread::sleep_for(max_threadup_spinup_time);
+
    std::vector<paired_time> runtimes;
    runtimes.reserve(n);
    for (int i = 0; i < n; ++i) {
       if (i % 100 == 0)
          std::cout << 100 * i / n << "% ";
+      if(i==n-1)
+         should_end.store(true);
       runtimes.emplace_back(measure());
    }
    std::cout << "\n";
    report(runtimes, "semaphore_latency");
+   
 }
