b302cb9ad4e6c6514fc76f7e9de49ceb11408a8a
Sebastian Werhausen <swerhausen@gmail.com>
2021-12-12 21:20:52 +0100

    unifying things

--- semaphore_latency.cpp
+++ semaphore_latency.cpp
@@ -1,75 +1,16 @@
 #include "semaphore_latency.h"
 
 #include <semaphore>
-#include <thread>
-#include <iostream>
 
 #include "tools.h"
 
-namespace {
-   std::binary_semaphore semaphore{ 0 };
-   struct thread_result
-   {
-      std::chrono::high_resolution_clock::time_point t1{};
-      int processor_number{};
-   };
-   std::atomic_bool should_end = false;
-   std::atomic_bool armed = false;
-   std::atomic<std::optional<thread_result>> t1;
-
-
-   auto thread_fun() -> void
-   {
-      while(should_end.load() == false)
-      {
-         armed.wait(false);
-
-         semaphore.acquire();
-         const auto time = std::chrono::high_resolution_clock::now();
-
-         t1.store(thread_result{ time, 42 });
-         armed.store(false);
-      }
-      std::cout << "ended\n";
-   }
-
-   auto measure() -> paired_time {
-      armed.store(true);
-      armed.notify_one();
-      std::this_thread::sleep_for(max_thread_write_time);
-
-      const auto t0 = std::chrono::high_resolution_clock::now();
-      semaphore.release();
-      std::this_thread::sleep_for(max_thread_write_time);
-
-      
-      if (t1.load().has_value() == false)
-         std::terminate();
-
-      const auto loaded = t1.load();
-      const double ns = std::chrono::duration_cast<dbl_ns>(loaded->t1 - t0).count();
-
-      t1.store(std::nullopt);
-      const auto processor_number = static_cast<int>(GetCurrentProcessorNumber());
-      return paired_time{ processor_number, loaded->processor_number, ns };
-   }
-
-}
 
 auto measure_semaphore_latency(const int n) -> void {
-   std::jthread j(thread_fun);
-   std::this_thread::sleep_for(max_threadup_spinup_time);
+   std::binary_semaphore semaphore{ 0 };
 
-   std::vector<paired_time> runtimes;
-   runtimes.reserve(n);
-   for (int i = 0; i < n; ++i) {
-      if (i % 100 == 0)
-         std::cout << 100 * i / n << "% ";
-      if(i==n-1)
-         should_end.store(true);
-      runtimes.emplace_back(measure());
-   }
-   std::cout << "\n";
-   report(runtimes, "semaphore_latency");
+   const auto tick_fun = [&]() {semaphore.release(); };
+   const auto tock_fun = [&]() {semaphore.acquire(); };
    
+   measurer m;
+   m.start(n, tick_fun, tock_fun);
 }
--- tools.h
+++ tools.h
@@ -1,30 +1,57 @@
 #pragma once
 
-#include <vector>
-#include <string>
 #include <fstream>
 #include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
 
 #include <chrono>
 using namespace std::chrono_literals;
-using dbl_ns = std::chrono::duration<double, std::nano>;
-using dbl_ms = std::chrono::duration<double, std::milli>;
-
-#define WIN32_LEAN_AND_MEAN 
-#include <windows.h>
+using result_unit = typename std::chrono::nanoseconds::rep;
 
 
 inline auto max_threadup_spinup_time = 5.0ms;
-constexpr auto max_thread_write_time = 100us;
+constexpr auto max_latency = 50us;
 
-struct paired_time{
-   int number0{};
-   int number1{};
-   double ns{};
-};
+//template<typename tick_fun_type>
+//[[nodiscard]] auto generic_measure(
+//   std::atomic_bool& armed,
+//   std::atomic<std::optional<std::chrono::high_resolution_clock::time_point>>& t1,
+//   const tick_fun_type& tick
+//) -> result_unit
+//{
+//   // Arm the thread so it runs to it's tock() function
+//   armed.store(true);
+//   armed.notify_one();
+//
+//   // Make sure the message went through
+//   std::this_thread::sleep_for(max_latency);
+//
+//   // Start the measurement
+//   const auto t0 = std::chrono::high_resolution_clock::now();
+//   tick(); // semaphore.release();
+//
+//   // Wait until the tock function is guaranteed to have finished
+//   std::this_thread::sleep_for(max_latency);
+//
+//   // Retrieve the stored timepoint
+//   const auto loaded_t1 = t1.load();
+//   if (loaded_t1.has_value() == false) {
+//      std::cout << "max_latency probably wasn't high enough\n";
+//      std::terminate();
+//   }
+//
+//   // Calculate result and reset things
+//   const auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(*loaded_t1 - t0).count();
+//   t1.store(std::nullopt);
+//   return ns;
+//}
+
+
 
 inline auto report(
-   const std::vector<paired_time>& vec,
+   const std::vector<result_unit>& vec,
    const char* description
 ) -> void
 {
@@ -34,36 +61,149 @@ inline auto report(
    filename += ".txt";
 
    std::ofstream filestream(filename);
-   for (const paired_time& value : vec)
-      filestream << std::to_string(value.ns) << " " << std::to_string(value.number0) << " " << std::to_string(value.number1) << "\n";
-}
-
-inline auto report(
-   const std::vector<double>& vec,
-   const char* description
-) -> void
-{
-   std::cout << "written " << description << "\n";
-   std::string filename = "python/";
-   filename += description;
-   filename += ".txt";
-   
-   std::ofstream filestream(filename);
-   for (const double value : vec)
+   for (const result_unit value : vec)
       filestream << std::to_string(value) << "\n";
 }
 
-inline auto report(
-   const std::vector<std::pair<double, double>>& vec,
-   const char* description
-) -> void
+struct measurer
 {
-   std::cout << "written " << description << "\n";
-   std::string filename = "python/";
-   filename += description;
-   filename += ".txt";
+   std::atomic_bool m_should_end = false;
+   std::atomic_bool m_armed = false;
+   std::atomic<std::optional<std::chrono::high_resolution_clock::time_point>> m_t1;
 
-   std::ofstream filestream(filename);
-   for (const auto& [row0, row1]: vec)
-      filestream << std::to_string(row0) << " " << row1 << "\n";
-}
+   template<typename tock_fun_type>
+   auto thread_fun(
+      const tock_fun_type& tock_fun
+   ) -> void
+   {
+      while (true)
+      {
+         // Wait until the measurement is "armed"
+         m_armed.wait(false);
+
+         // Do whatever is being measured on the spawned thread. Typically
+         // semaphore.acquire(), mutex.lock(), condition_variable.wait(), atomic_flag.wait()
+         tock_fun();
+
+         // Capture the timestamp and store it in the result variable
+         const auto time = std::chrono::high_resolution_clock::now();
+         m_t1.store(time);
+
+         // Disarm this thread until the Bookkeeping is done and this thread is re-armed again
+         m_armed.store(false);
+
+         // If enough measurements are done, this signal is being switched to true
+         if (m_should_end.load() == true)
+            return;
+      }
+   }
+
+
+   template<typename tick_fun_type>
+   [[nodiscard]] auto generic_measure(
+      const tick_fun_type& tick_fun
+   ) -> result_unit
+   {
+      // Arm the thread so it runs to it's tock() function
+      m_armed.store(true);
+      m_armed.notify_one();
+
+      // Make sure the message went through
+      std::this_thread::sleep_for(max_latency);
+
+      // Start the measurement
+      const auto t0 = std::chrono::high_resolution_clock::now();
+      tick_fun();
+
+      // Wait until the tock function is guaranteed to have finished
+      std::this_thread::sleep_for(max_latency);
+
+      // Retrieve the stored timepoint
+      const auto loaded_t1 = m_t1.load();
+      if (loaded_t1.has_value() == false) {
+         std::cout << "max_latency probably wasn't high enough\n";
+         std::terminate();
+      }
+
+      // Calculate result and reset things
+      const auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(*loaded_t1 - t0).count();
+      m_t1.store(std::nullopt);
+      return ns;
+   }
+
+
+   template<typename tick_fun_type, typename tock_fun_type>
+   auto start(
+      const int n,
+      const tick_fun_type& tick_fun,
+      const tock_fun_type& tock_fun
+   )
+   {
+      std::jthread j(
+         [&]() {thread_fun(tock_fun); }
+      );
+      std::this_thread::sleep_for(max_threadup_spinup_time);
+
+      std::vector<result_unit> runtimes;
+      runtimes.reserve(n);
+      for (int i = 0; i < n; ++i) {
+         if (i % 100 == 0)
+            std::cout << 100 * i / n << "% ";
+         if (i == n - 1)
+            m_should_end.store(true);
+         runtimes.emplace_back([&]() {return generic_measure(tick_fun); }());
+      }
+      std::cout << "\n";
+      report(runtimes, "semaphore_latency");
+   }
+
+};
+
+
+
+
+
+//inline auto report(
+//   const std::vector<paired_time>& vec,
+//   const char* description
+//) -> void
+//{
+//   std::cout << "written " << description << "\n";
+//   std::string filename = "python/";
+//   filename += description;
+//   filename += ".txt";
+//
+//   std::ofstream filestream(filename);
+//   for (const paired_time& value : vec)
+//      filestream << std::to_string(value.ns) << " " << std::to_string(value.number0) << " " << std::to_string(value.number1) << "\n";
+//}
+//
+//inline auto report(
+//   const std::vector<double>& vec,
+//   const char* description
+//) -> void
+//{
+//   std::cout << "written " << description << "\n";
+//   std::string filename = "python/";
+//   filename += description;
+//   filename += ".txt";
+//   
+//   std::ofstream filestream(filename);
+//   for (const double value : vec)
+//      filestream << std::to_string(value) << "\n";
+//}
+//
+//inline auto report(
+//   const std::vector<std::pair<double, double>>& vec,
+//   const char* description
+//) -> void
+//{
+//   std::cout << "written " << description << "\n";
+//   std::string filename = "python/";
+//   filename += description;
+//   filename += ".txt";
+//
+//   std::ofstream filestream(filename);
+//   for (const auto& [row0, row1]: vec)
+//      filestream << std::to_string(row0) << " " << row1 << "\n";
+//}
